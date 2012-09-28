ac90f2d08828beef002e392efe06a8dc14159a3c
Vinodh Rajagopal <vinodhsamurai@gmail.com>
2012-09-28 08:59:09 -0700

    Changed Build.scala to use github repo for html5 module

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object ApplicationBuild extends Build {
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       // Add your own project settings here 
-      resolvers += "repo at github" at "https://github.com/vinodhrajagopal/Play2-HTML5Tags/blob/master"
+      resolvers += Resolver.url("My GitHub Repository", url("http://vinodhrajagopal.github.com/repository/"))(Resolver.ivyStylePatterns))
       //resolvers += "Local Play Repository" at "file:///home/vinodh/play-2.0.3/repository"
       //resolvers += "Local Play Repository" at "file:///Users/vrajagopal/softwares/play-2.0.3/repository"     
     )
